d153f333e16423d147dbfb6f6aa49b1f35bbd8c2
10zx <65040554+10zx@users.noreply.github.com>
2020-11-05 08:02:10 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+from pynput.keyboard import Key, Controller
+import time
+
+keyboard = Controller()
+
+time.sleep(3)
+
+while True:
+    time.sleep(1)
+
+    # Key in !d bump
+    keyboard.press('!')
+    keyboard.release('!')
+    keyboard.press('d')
+    keyboard.release('d')
+    keyboard.press(Key.space)
+    keyboard.release(Key.space)
+    keyboard.press('b')
+    keyboard.release('b')
+    keyboard.press('u')
+    keyboard.release('u')
+    keyboard.press('m')
+    keyboard.release('m')
+    keyboard.press('p')
+    keyboard.release('p')
+
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    
+    print('server !d bumped')
+
+    for i in range(7210, 0, -1):
+        time.sleep(1)
+        print(f'sleeping for {i} more seconds')
+
+
+
+
